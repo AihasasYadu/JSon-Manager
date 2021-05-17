efffeffb12d45bfb6a8e77227ed78082e05dc2a6
Aihasas Yadu <yadu.aihasas@gmail.com>
2021-05-18 00:59:16 +0530

    Managing Jsons using URLs

---
+++ Assets/Scripts/JSonManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class Interest
+{
+    public string Name;
+    public string PictureUrl;
+    public string DisplayName;
+    public string Language;
+    public int InterestID;
+}
+
+[System.Serializable]
+public class InterestList
+{
+    public Interest[] interest;
+}
+
+public class JSonManager : MonoSingletonGeneric<JSonManager>
+{
+    public string jsonURL;
+    private string path;
+    public InterestList interestData;
+
+    private void Start()
+    {
+        path = Application.dataPath;
+    }
+
+    public void SaveData()
+    {
+        StartCoroutine(GetData());
+    }
+
+    private IEnumerator GetData()
+    {
+        WWW web = new WWW(jsonURL);
+        yield return web;
+        if (web.error == null)
+        {
+            SaveData(web.text);
+        }
+        else
+        {
+            Debug.Log("Error");
+        }
+    }
+
+    private void SaveData(string jsonData)
+    {
+        interestData = JsonUtility.FromJson<InterestList>("{\"interest\":" + jsonData + "}");
+        string dataToSave = JsonUtility.ToJson(interestData);
+        File.WriteAllText(path + "/JSONs/Interests.json", dataToSave);
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private Button fetchButton,
+                                    showButton,
+                                    nextButton,
+                                    previousButton;
+    [SerializeField] private RectTransform showDataPanel;
+    [SerializeField] private TextMeshProUGUI nameText,
+                                             displayNameText,
+                                             languageText,
+                                             interestIDText;
+    [SerializeField] private Image picture;
+
+    private InterestList data;
+    private int i = 0;
+    private int dataLength;
+
+    private void Start()
+    {
+        fetchButton.onClick.AddListener(FetchJsonData);
+        showButton.onClick.AddListener(ShowJsonData);
+        nextButton.onClick.AddListener(PreviewNext);
+        previousButton.onClick.AddListener(PreviewPrevious);
+        JSonManager.Instance.SaveData();
+    }
+
+    private void FetchJsonData()
+    {
+        data = JSonManager.Instance.interestData;
+        dataLength = data.interest.Length;
+        ColorBlock c = fetchButton.colors;
+        c.normalColor = Color.green;
+        c.selectedColor = Color.green;
+        fetchButton.colors = c;
+        fetchButton.onClick.RemoveListener(FetchJsonData);
+    }
+
+    private void ShowJsonData()
+    {
+        showDataPanel.gameObject.SetActive(true);
+        UpdateDataOnCanvas();
+    }
+
+    private void PreviewNext()
+    {
+        if (i < dataLength - 1 && i > -1)
+            ++i;
+        else
+            i = 0;
+
+        UpdateDataOnCanvas();
+    }
+
+    private IEnumerator SetPic(string url)
+    {
+        WWW web = new WWW(url);
+        yield return web;
+        if (web.error == null && web.texture != null)
+        {
+            picture.sprite = Sprite.Create(web.texture, new Rect(0, 0, web.texture.width, web.texture.height), new Vector2(0, 0));
+        }
+    }
+
+    private void PreviewPrevious()
+    {
+        if(i > 1 && i < dataLength)
+            --i;
+        else
+            i = dataLength - 1;
+
+        UpdateDataOnCanvas();
+    }
+    
+    private void UpdateDataOnCanvas()
+    {
+        nameText.SetText(data.interest[i].Name);
+        displayNameText.SetText(data.interest[i].DisplayName);
+        languageText.SetText(data.interest[i].Language);
+        interestIDText.SetText(data.interest[i].InterestID.ToString());
+        StartCoroutine(SetPic(data.interest[i].PictureUrl));
+    }
+}
